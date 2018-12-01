035f27df5b5ad48b93d831cbb8ee8d52b9862075
Madhusudan Sharma <madhusudan1997.sharma@gmail.com>
2018-12-01 22:42:47 +0530

    Fix exoplayer state on screen orientation change, add video cache

--- app/src/main/java/com/magarex/bigchef/ui/exoplayer/ExoPlayerFragment.java
+++ app/src/main/java/com/magarex/bigchef/ui/exoplayer/ExoPlayerFragment.java
@@ -1,5 +1,6 @@
 package com.magarex.bigchef.ui.exoplayer;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
@@ -14,20 +15,26 @@ import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.upstream.FileDataSource;
+import com.google.android.exoplayer2.upstream.cache.CacheDataSink;
+import com.google.android.exoplayer2.upstream.cache.CacheDataSource;
+import com.google.android.exoplayer2.util.Util;
 import com.magarex.bigchef.R;
 import com.magarex.bigchef.databinding.FragmentExoPlayerBinding;
 import com.magarex.bigchef.model.Step;
 import com.magarex.bigchef.ui.base.BaseFragment;
 
-import java.util.List;
 import java.util.Objects;
 
 public class ExoPlayerFragment extends BaseFragment<FragmentExoPlayerBinding> {
@@ -35,6 +42,7 @@ public class ExoPlayerFragment extends BaseFragment<FragmentExoPlayerBinding> {
     private Step mStep;
     private SimpleExoPlayer exoPlayer;
     private Boolean isTablet = false;
+    static boolean playerState = true;
 
     public ExoPlayerFragment() {
     }
@@ -42,9 +50,8 @@ public class ExoPlayerFragment extends BaseFragment<FragmentExoPlayerBinding> {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState != null) {
+        if (savedInstanceState != null)
             mStep = savedInstanceState.getParcelable(getString(R.string.steps));
-        }
     }
 
     @Nullable
@@ -67,10 +74,8 @@ public class ExoPlayerFragment extends BaseFragment<FragmentExoPlayerBinding> {
                     ConstraintLayout.LayoutParams errorParams = (ConstraintLayout.LayoutParams) getDataBinding().ivError.getLayoutParams();
                     errorParams.height = ConstraintLayout.LayoutParams.MATCH_CONSTRAINT;
                 }
-            } else {
+            } else
                 prepareView(savedInstanceState);
-            }
-
         } else Toast.makeText(getActivity(), "No Steps Available", Toast.LENGTH_SHORT).show();
     }
 
@@ -78,29 +83,60 @@ public class ExoPlayerFragment extends BaseFragment<FragmentExoPlayerBinding> {
         getDataBinding().setStep(mStep);
         initializePlayer();
         getDataBinding().recipePlayerView.setPlayer(exoPlayer);
-        if (savedInstanceState != null && savedInstanceState.getLong(getString(R.string.player_current_position)) != 0) {
+        if (savedInstanceState != null && savedInstanceState.getLong(getString(R.string.player_current_position)) != 0)
             exoPlayer.seekTo(savedInstanceState.getLong(getString(R.string.player_current_position)));
-            exoPlayer.setPlayWhenReady(savedInstanceState.getBoolean(getString(R.string.player_state)));
-        }
     }
 
     private void initializePlayer() {
         DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        TrackSelection.Factory videoTrackSelectionFactory =
-                new AdaptiveTrackSelection.Factory(bandwidthMeter);
-        DefaultTrackSelector trackSelector =
-                new DefaultTrackSelector(videoTrackSelectionFactory);
-        exoPlayer = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector);
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        DefaultTrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+        exoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector);
         Uri uri = Uri.parse(mStep.getVideoURL());
         MediaSource mediaSource = buildMediaSource(uri);
         exoPlayer.prepare(mediaSource, true, false);
         exoPlayer.setPlayWhenReady(true);
+        exoPlayer.addListener(new Player.DefaultEventListener() {
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                playerState = playWhenReady;
+            }
+        });
     }
 
     private MediaSource buildMediaSource(Uri uri) {
-        return new ExtractorMediaSource
-                .Factory(new DefaultHttpDataSourceFactory(requireContext().getPackageName()))
-                .createMediaSource(uri);
+        return new ExtractorMediaSource.Factory(
+                new CacheDataSourceFactory(getContext(), 1024 * 1024 * 1024)
+        ).createMediaSource(uri);
+    }
+
+    class CacheDataSourceFactory implements DataSource.Factory {
+        private final Context context;
+        private final DefaultDataSourceFactory defaultDatasourceFactory;
+        private final long maxFileSize;
+
+        CacheDataSourceFactory(Context context, long maxFileSize) {
+            super();
+            this.context = context;
+            this.maxFileSize = maxFileSize;
+            String userAgent = Util.getUserAgent(context, context.getString(R.string.app_name));
+            DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            defaultDatasourceFactory = new DefaultDataSourceFactory(
+                    Objects.requireNonNull(getContext()),
+                    bandwidthMeter,
+                    new DefaultHttpDataSourceFactory(userAgent, bandwidthMeter));
+        }
+
+        @Override
+        public DataSource createDataSource() {
+            return new CacheDataSource(
+                    VideoCache.getInstance(context),
+                    defaultDatasourceFactory.createDataSource(),
+                    new FileDataSource(),
+                    new CacheDataSink(VideoCache.getInstance(context), maxFileSize),
+                    CacheDataSource.FLAG_BLOCK_ON_CACHE | CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR,
+                    null);
+        }
     }
 
     public void setStep(Step step) {
@@ -122,10 +158,8 @@ public class ExoPlayerFragment extends BaseFragment<FragmentExoPlayerBinding> {
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        if (exoPlayer != null && exoPlayer.getCurrentPosition() != 0) {
+        if (exoPlayer != null && exoPlayer.getCurrentPosition() != 0)
             outState.putLong(getString(R.string.player_current_position), exoPlayer.getCurrentPosition());
-            outState.putBoolean(getString(R.string.player_state), exoPlayer.getPlayWhenReady());
-        }
         outState.putParcelable(getString(R.string.steps), mStep);
     }
 
@@ -143,9 +177,8 @@ public class ExoPlayerFragment extends BaseFragment<FragmentExoPlayerBinding> {
     @Override
     public void onPause() {
         super.onPause();
-        if (Build.VERSION.SDK_INT < 24) {
+        if (Build.VERSION.SDK_INT < 24)
             releasePlayer();
-        }
     }
 
     @Override
@@ -158,31 +191,26 @@ public class ExoPlayerFragment extends BaseFragment<FragmentExoPlayerBinding> {
     @Override
     public void onStop() {
         super.onStop();
-        if (Build.VERSION.SDK_INT >= 24) {
+        if (Build.VERSION.SDK_INT >= 24)
             releasePlayer();
-        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        if (exoPlayer == null) {
+        if (exoPlayer == null)
             initializePlayer();
-        }
     }
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
         if (this.isVisible()) {
-            if (!isVisibleToUser) {
-                exoPlayer.setPlayWhenReady(false);
+            if (!isVisibleToUser)
                 exoPlayer.getPlaybackState();
-            }
-            if (isVisibleToUser) {
-                exoPlayer.setPlayWhenReady(true);
+            if (isVisibleToUser)
                 exoPlayer.getPlaybackState();
-            }
+            exoPlayer.setPlayWhenReady(playerState);
         }
     }
 }
